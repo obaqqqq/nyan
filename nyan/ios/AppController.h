8bae14c7687a211e25e4140ef5d1e778890d23b8
Shinsuke Kasahara <obaqqqq@gmail.com>
2013-10-02 01:34:42 +0900

    Initial Commit

---
+++ nyan/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  nyanAppController.h
+//  nyan
+//
+//  Created by 笠原 新介 on 13/10/02.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ nyan/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  nyanAppController.h
+//  nyan
+//
+//  Created by 笠原 新介 on 13/10/02.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
